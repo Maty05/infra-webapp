4674bd4dacebd8d792179955eaa815d9c8f5ef32
Mauro Schinca <mschinca@frba.utn.edu.ar>
2023-11-15 20:33:24 -0300

    Primer commit con webapp

---
+++ server.py
@@ -0,0 +1,16 @@
+from flask import Flask
+import sys
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hola Mundo!'
+
+@app.route('/romper')
+def romper():
+    os._exit(1)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
